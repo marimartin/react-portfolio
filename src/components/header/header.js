1a5e10bae247b33ec7e9cb9634ca48b7c37e2339
Mari <marinarosemartin@gmail.com>
2020-12-06 16:41:13 -0600

    header completed

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -13,18 +13,17 @@ export default class Header extends Component {
                             <li><a className="smoothscroll" href="#about">About</a></li>
                             <li><a className="smoothscroll" href="#resume">Resume</a></li>
                             <li><a className="smoothscroll" href="#portfolio">Works</a></li>
-                            <li><a className="smoothscroll" href="#testimonials">Testimonials</a></li>
                             <li><a className="smoothscroll" href="#contact">Contact</a></li>
                         </ul> {/* end #nav */}
                     </nav> {/* end #nav-wrap */}
                     <div className="row banner">
                         <div className="banner-text">
                             <h1 className="responsive-headline">Marina Martin</h1>
-                            <h3>I'm a Chicago based Full-Stack Web Developer, with a background in Fine Arts. I love making beaufiul things and solving problems. Let'sstart scrolling and learn more <a className="smoothscroll" href="#about">about me</a>.</h3>
+                            <h3>I'm a Chicago based Full-Stack Web Developer, with a background in Fine Arts. I love making beaufiul things and solving problems. Let's start scrolling and learn more <a className="smoothscroll" href="#about">about me</a>.</h3>
                             <hr />
                             <ul className="social">
-                                <li><a href="https://linkedin.com/in/marina-martin6"><i className="fa fa-linkedin" /></a></li>
-                                <li><a href="https://github.com/marimartin"><i className="fa fa-github" /></a></li>
+                                <li><a href="https://linkedin.com/in/marina-martin6"><i className="fa fa-linkedin fa-2x" /></a></li>
+                                <li><a href="https://github.com/marimartin"><i className="fa fa-github fa-2x" /></a></li>
                             </ul>
                         </div>
                     </div>
