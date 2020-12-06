9f3824155ff5a9390cb09b95da2a69c10875a1ed
Mari <marinarosemartin@gmail.com>
2020-12-06 17:01:27 -0600

    resume props

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 export default class Header extends Component {
     render() {
+        let resumeData = this.props.resumeData;
         return (
             <React.Fragment>
                 {/*generated code*/}
                 <header id="home">
+
                     <nav id="nav-wrap">
                         <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
                         <a className="mobile-btn" href="#" title="Hide navigation">Hide navigation</a>
@@ -18,8 +20,10 @@ export default class Header extends Component {
                     </nav> {/* end #nav-wrap */}
                     <div className="row banner">
                         <div className="banner-text">
-                            <h1 className="responsive-headline">Marina Martin</h1>
-                            <h3>I'm a Chicago based Full-Stack Web Developer, with a background in Fine Arts. I love making beaufiul things and solving problems. Let's start scrolling and learn more <a className="smoothscroll" href="#about">about me</a>.</h3>
+                            <h1 className="responsive-headline">{resumeData.name}</h1>
+                            <h3>I am a Chicago based {resumeData.role} with a background in Fine Arts. I love making beautiful things and solving problems.</h3>
+                            <h3>Let's start scrolling and learn more <a className="smoothscroll" href="#about">about me</a>.
+                            </h3>
                             <hr />
                             <ul className="social">
                                 <li><a href="https://linkedin.com/in/marina-martin6"><i className="fa fa-linkedin fa-2x" /></a></li>
---
+++ src/resumeData.js
@@ -0,0 +1,6 @@
+let resumeData = {
+    "imagebaseurl": "http://localhost:3000/",
+    "name": "Marina Martin",
+    "role": "Full-Stack Web Developer"
+}
+export default resumeData
