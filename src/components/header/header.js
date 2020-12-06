efcf14c833d3a3aeb443c9981f21e1f8c4126b54
Mari <marinarosemartin@gmail.com>
2020-12-06 15:07:33 -0600

    header edited

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -19,19 +19,12 @@ export default class Header extends Component {
                     </nav> {/* end #nav-wrap */}
                     <div className="row banner">
                         <div className="banner-text">
-                            <h1 className="responsive-headline">I'm Jonathan Doe.</h1>
-                            <h3>I'm a Manila based <span>graphic designer</span>, <span>illustrator</span> and <span>webdesigner</span> creating awesome and
-              effective visual identities for companies of all sizes around the globe. Let's <a className="smoothscroll" href="#about">start scrolling</a>
-              and learn more <a className="smoothscroll" href="#about">about me</a>.</h3>
+                            <h1 className="responsive-headline">Marina Martin</h1>
+                            <h3>I'm a Chicago based Full-Stack Web Developer, with a background in Fine Arts. I love making beaufiul things and solving problems. Let'sstart scrolling and learn more <a className="smoothscroll" href="#about">about me</a>.</h3>
                             <hr />
                             <ul className="social">
-                                <li><a href="#"><i className="fa fa-facebook" /></a></li>
-                                <li><a href="#"><i className="fa fa-twitter" /></a></li>
-                                <li><a href="#"><i className="fa fa-google-plus" /></a></li>
-                                <li><a href="#"><i className="fa fa-linkedin" /></a></li>
-                                <li><a href="#"><i className="fa fa-instagram" /></a></li>
-                                <li><a href="#"><i className="fa fa-dribbble" /></a></li>
-                                <li><a href="#"><i className="fa fa-skype" /></a></li>
+                                <li><a href="https://linkedin.com/in/marina-martin6"><i className="fa fa-linkedin" /></a></li>
+                                <li><a href="https://github.com/marimartin"><i className="fa fa-github" /></a></li>
                             </ul>
                         </div>
                     </div>
