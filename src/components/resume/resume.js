1f13ac5dcebe5c84a2d5527d74a5834fd49470bd
Mari <marinarosemartin@gmail.com>
2020-12-07 00:30:50 -0600

    skills

--- src/components/resume/resume.js
+++ src/components/resume/resume.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+
 export default class Header extends Component {
     render() {
         return (
@@ -97,20 +98,32 @@ export default class Header extends Component {
                             <h1><span>Skills</span></h1>
                         </div>
                         <div className="nine columns main-col">
-                            <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam,
+                            {/* <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam,
                             eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam
                             voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione
                             voluptatem sequi nesciunt.
-            </p>
+                            </p> */}
                             <div className="bars">
-                                <ul className="skills">
-                                    <li><span className="bar-expand photoshop" /><em>Photoshop</em></li>
-                                    <li><span className="bar-expand illustrator" /><em>Illustrator</em></li>
-                                    <li><span className="bar-expand wordpress" /><em>Wordpress</em></li>
-                                    <li><span className="bar-expand css" /><em>CSS</em></li>
-                                    <li><span className="bar-expand html5" /><em>HTML5</em></li>
-                                    <li><span className="bar-expand jquery" /><em>jQuery</em></li>
-                                </ul>
+                                <div class="row">
+                                    <div className="column">
+                                        <ul className="skills">
+                                            <li><em>Reactjs</em></li>
+                                            <li><em>JavaScript</em></li>
+                                            <li><em>SQL</em></li>
+                                            <li><em>NoSQL</em></li>
+                                            <li><em>AJAX</em></li>
+                                        </ul>
+                                    </div>
+                                    <div className="column">
+                                        <ul className="skills">
+                                            <li><em>Nodejs</em></li>
+                                            <li><em>CSS</em></li>
+                                            <li><em>HTML5</em></li>
+                                            <li><em>jQuery</em></li>
+                                            <li><em>Bootstrap</em></li>
+                                        </ul>
+                                    </div>
+                                </div>
                             </div>{/* end skill-bars */}
                         </div> {/* main-col end */}
                     </div> {/* End skills */}
